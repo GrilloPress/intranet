0136b14cde18d1e275da53a804f5eaafe63cc354
Andrew Duckworth <grillopress@gmail.com>
2017-03-10 21:57:43 +0000

    add branded headers

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source 'https://rubygems.org'
 gem "middleman", "~>3.3.12"
 gem "middleman-livereload", "~> 3.1.0"
 gem 'bootstrap-sass', require: false
+gem 'font-awesome-sass', require: false
--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ set :images_dir, 'images'
 
 configure :build do
 
-  activate :minify_css
+  # activate :minify_css
   activate :minify_javascript
   activate :relative_assets
   set :relative_links, true
